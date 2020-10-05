85b434dbc1782b04a22cc96dfdbaf844774e4002
Ivansukma <ivanrzr12@gmail.com>
2020-10-05 15:00:59 +0700

    pushh brooo ivan

--- Perpus/src/views/Pegawai.java
+++ Perpus/src/views/Pegawai.java
@@ -155,9 +155,7 @@ public class Pegawai extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_userinActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
--- Perpus/src/views/PegawaiView.java
+++ Perpus/src/views/PegawaiView.java
@@ -371,11 +371,9 @@ public class PegawaiView extends javax.swing.JFrame {
         setNetralCondition();
     }//GEN-LAST:event_brshtmblActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -397,15 +395,7 @@ public class PegawaiView extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(PegawaiView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new PegawaiView().setVisible(true);
