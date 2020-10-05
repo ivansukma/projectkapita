4179fdcf1734ea968084c08b63c027509d9f9092
robertusbagaskara <robertusbagaskara@gmail.com>
2020-10-05 21:28:45 +0700

    MySQL Source

--- Perpus/src/tools/Koneksi.java
+++ Perpus/src/tools/Koneksi.java
@@ -5,27 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class Koneksi {
     
     public Connection con = null;
-    public String Driver;
-    public String url;
-    public String user;
-    public String pass;
     
     public Koneksi(){
-        Driver = "com.mysql.jdbc.Driver";
-        url = "jdbc:mysql://localhost:3306/db_perpus";
-        user = "root";
-        pass = "";
-        try {
-            Class.forName(Driver);
-            con = DriverManager.getConnection(url, user, pass);
-        } catch (SQLException e) {
-            Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, e);
-        } catch (ClassNotFoundException e) {
-            Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, e);
+        try
+        {
+            con=DriverManager.getConnection("jdbc:mysql://localhost/db_perpus","root","");
+            JOptionPane.showMessageDialog(null,"connected with "+con.toString());
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,"not connect to server and message is"+e.getMessage());
         }
     }
     
